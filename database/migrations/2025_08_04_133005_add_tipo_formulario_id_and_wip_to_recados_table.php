1685a5594f17fc69ad7a62a9c5b265ec3c87ab08
goncalo <goncalo.cardoso@soccsantos.pt>
2025-09-30 10:33:53 +0100

    corrigir erros;

--- database/migrations/2025_08_04_133005_add_tipo_formulario_id_and_wip_to_recados_table.php
+++ database/migrations/2025_08_04_133005_add_tipo_formulario_id_and_wip_to_recados_table.php
@@ -8,14 +8,13 @@ return new class extends Migration {
     public function up(): void {
         Schema::table('recados', function (Blueprint $table) {
             $table->foreignId('tipo_formulario_id')->nullable()->constrained('tipo_formularios');
-            $table->string('wip')->nullable(); // Só será usado em "Call Center"
         });
     }
 
     public function down(): void {
         Schema::table('recados', function (Blueprint $table) {
             $table->dropForeign(['tipo_formulario_id']);
-            $table->dropColumn(['tipo_formulario_id', 'wip']);
+            $table->dropColumn(['tipo_formulario_id']);
         });
     }
 };
