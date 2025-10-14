c7817802319633650f4d22ccf742058216eff9a5
goncalo <goncalo.cardoso@soccsantos.pt>
2025-10-14 14:18:03 +0100

    corrigir migration para adicionar user_id na tabela recados

--- database/migrations/2025_10_13_153542_add_user_id_to_recados_table.php
+++ database/migrations/2025_10_13_153542_add_user_id_to_recados_table.php
@@ -1,26 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
-{
-    Schema::table('recados', function (Blueprint $table) {
-        $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
-    });
-}
+    public function up(): void
+    {
+        Schema::table('recados', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+        });
+    }
 
-public function down()
-{
-    Schema::table('recados', function (Blueprint $table) {
-        $table->dropConstrainedForeignId('user_id');
-    });
-}
-
+    public function down(): void
+    {
+        Schema::table('recados', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
 };
